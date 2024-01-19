a57254bcf78801526d25890669d6e3604acf217f
Arun1only1 <arun1nly1@gmail.com>
2024-01-19 16:05:37 +0545

    course crud

--- course/course.route.js
+++ course/course.route.js
@@ -46,4 +46,90 @@ router.get("/course/details/:id", async (req, res) => {
   return res.status(200).send(course);
 });
 
+// delete course
+router.delete("/course/delete/:id", async (req, res) => {
+  // extract id from req.params
+  const courseId = req.params.id;
+
+  // check for mongo id validity
+  const isValidMongoId = checkMongoIdValidity(courseId);
+
+  // if not valid mongo id, throw error
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // find course
+  const course = await Course.findOne({ _id: courseId });
+
+  // if not course, throw error
+  if (!course) {
+    return res.status(404).send({ message: "Course does not exist." });
+  }
+
+  // delete course
+  await Course.deleteOne({ _id: courseId });
+
+  return res.status(200).send({ message: "Course is deleted successfully." });
+});
+
+// edit course
+router.put("/course/edit/:id", async (req, res) => {
+  // extract id from req.params
+  const courseId = req.params.id;
+
+  // check for mongo id validity
+
+  const isValidMongoId = checkMongoIdValidity(courseId);
+
+  // if not valid mongo id, throw error
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // find course
+  const requiredCourse = await Course.findOne({ _id: courseId });
+
+  // if not course, throw error
+  if (!requiredCourse) {
+    return res.status(404).send({ message: "Course does not exist." });
+  }
+
+  // extract new values from req.body
+  const newValues = req.body;
+
+  // edit course
+  await Course.updateOne(
+    { _id: courseId },
+    {
+      $set: {
+        name: newValues.name,
+        price: newValues.price,
+        duration: newValues.duration,
+        tutorName: newValues.tutorName,
+      },
+    }
+  );
+
+  // send response
+  return res.status(200).send({ message: "Course is edited successfully." });
+});
+
+// get course list
+router.post("/course/list", async (req, res) => {
+  const page = req.body.page;
+  const limit = req.body.limit;
+
+  // calculate skip
+  const skip = (page - 1) * limit;
+
+  const courseList = await Course.aggregate([
+    { $match: {} },
+    { $skip: skip },
+    { $limit: limit },
+  ]);
+
+  return res.status(200).send({ message: "success", courses: courseList });
+});
+
 export default router;
